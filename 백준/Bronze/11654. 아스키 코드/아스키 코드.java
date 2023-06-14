3d6c206cffb6465ac770f6ff866c5feffdf5cb1a
LEE5UM1N <133942411+LEE5UM1N@users.noreply.github.com>
2023-06-14 11:56:08 +0900

    [Bronze V] Title: 아스키 코드, Time: 208 ms, Memory: 17556 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+    	Scanner scan = new Scanner(System.in);
+    	
+    	char popo = 0;
+    	
+		popo=scan.next().charAt(0);
+		
+		int a=popo;
+		
+		System.out.println(a);
+    	
+    }
+}
